c2a4c95d4f84bd8da4e7507add32c0eec8b9fb64
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-11-22 11:15:45 +0000

    functions made for any change

--- To-Do React/src/App.jsx
+++ To-Do React/src/App.jsx
@@ -2,16 +2,63 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 
+import Navbar from './components/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const handleEdit = () => {
+
+  }
+  const handleDelete = (e) => {
+    // todos.filter(()=>
+    //   console
+    // )
+  }
+  const handleAdd = (e) => {
+    setTodos([...todos, { todo, isCompleted: false }])
+    setTodo("")
+    console.log(todos)
+  }
+  const handleOnChange = (e) => {
+    setTodo(e.target.value)
+    console.log(todo)
+  }
+  const handleCheckBox=(e)=>{
+    
+  }
+
 
   return (
     <>
-    <Navbar></Navbar>
-      <div className="con">
-        <div className="bg-red-100">heyyy</div>
+      <Navbar></Navbar>
+      <div className="container mx-auto bg-slate-600 m-10 p-4 text-white rounded-xl min-h-[80vh]">
+        <div className="addTodo">
+          <h1 className="text-2xl font-bold">Add a ToDo</h1>
+          <div>
+            <input onChange={handleOnChange} type="text" className='h-12 rounded-md w-1/2 my-5 text-black'></input>
+            <button onClick={handleAdd} className='bg-green-600 hover:bg-green-800 p-3 m-3 mx-6 rounded-md hover:font-bold'   >Add</button>
+          </div>
+        </div>
+        <h2 className='text-2xl font-bold'>Your ToDos</h2>
+        <div className="todos">
+          {
+            todos.map((item) =>
+
+              <div key={todo} className="todo flex items-center justify-between ">
+                <div className='flex'>
+                  <input type="checkbox" onChange={handleCheckBox} value={todos.isCompleted}></input>
+                  <div className={item.isCompleted && "line-through"}>{item.todo}</div>
+                </div>
+                <div className="buttons  float-end">
+                  <button onClick={handleEdit} className='bg-blue-600 hover:bg-blue-800 float-end p-2 px-4 m-3 mx-2 rounded-md hover:font-bold'>Edit</button>
+                  <button onClick={handleDelete} className='bg-red-600 hover:bg-red-800 p-2 float-end px-4 m-3 mx-2 rounded-md hover:font-bold'>Delete</button>
+                </div>
+              </div>
+            )}
+        </div>
+
       </div>
     </>
   )
--- To-Do React/src/components/Navbar.jsx
+++ To-Do React/src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Hey I am a navbar
+    <div className='bg-slate-900 text-white flex justify-between py-5' >
+      <div className="logo">
+        <span className='font-bold text-xl mx-9 my-0'>iTask</span>
+      </div>
+      <ul className='flex gap-8 mx-9 '>
+        <li className='cursor-pointer hover:font-extrabold transition-all duration-50ms'>Home</li>
+        <li className='cursor-pointer hover:font-extrabold transition-all duration-50ms'>To-do</li>
+      </ul>
     </div>
   )
 }
