57682fe3eeabd9ff142848f7902212c6db5572c7
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-11-22 09:19:54 +0000

    new todo created

---
+++ To-Do React/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar></Navbar>
+      <div className="con">
+        <div className="bg-red-100">heyyy</div>
+      </div>
+    </>
+  )
+}
+
+export default App
